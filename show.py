17f2633472e5de6c74ee1143f1a584d78483ed53
k2re8 <dns.1983n@gmail.com>
2023-07-24 14:35:05 +0300

    initial commit

---
+++ show.py
@@ -0,0 +1,41 @@
+#!/c/Python311/python
+
+import subprocess
+import time
+import os
+import sys
+
+def run_where(target) -> list:
+    proc = subprocess.Popen(['where', target], stdout=subprocess.PIPE)
+    targets = []
+
+    while True:
+        line = proc.stdout.readline().rstrip()      
+        if not line:
+            break
+        targets.append(line.decode())
+    
+    return targets
+
+def open_target(target):
+    targets = run_where(target)
+
+    if (not targets):
+        print(f'There are not targets for { target }')
+        exit(1)
+
+    index = 1
+    for target in targets:
+        print(f'[{index}]', target)
+        index += 1
+
+    path = targets[0]
+    # dir = os.path.dirname(path)
+
+    os.system(f'explorer /select,"{ path }"')
+
+if len(sys.argv) != 2:
+    print('No target to show')
+    exit(2)
+
+open_target(sys.argv[1])
